ab26fd104eecee84e686ff88b55bdbccc79275e0
Javid Sadigli <cavidoffical@gmail.com>
2025-07-21 23:23:43 +0400

    Added logging to console service

--- src/main/java/az/javidsadigli/portfolio/service/ConsoleService.java
+++ src/main/java/az/javidsadigli/portfolio/service/ConsoleService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 
 import az.javidsadigli.portfolio.storage.ConsoleStorage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ConsoleService 
@@ -13,6 +15,7 @@ public class ConsoleService
 
     public String getCommandOutput(String command)
     {
+        log.debug("Request received for command : {}", command);
         return consoleStorage.getCommandOutput(command);
     }
 }

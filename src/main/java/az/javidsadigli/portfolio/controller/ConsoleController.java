2757033ad0a93d53f7530c8eb256813358508e4c
Javid Sadigli <cavidoffical@gmail.com>
2025-07-21 23:19:30 +0400

    Added api for getting command outputs

---
+++ src/main/java/az/javidsadigli/portfolio/controller/ConsoleController.java
@@ -0,0 +1,28 @@
+package az.javidsadigli.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import az.javidsadigli.portfolio.service.ConsoleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/console")
+public class ConsoleController 
+{
+    private static final String LOG_TEMPLATE = "{} request to /api/v1/console{}";
+
+    private final ConsoleService consoleService; 
+
+    @GetMapping(value = "/command-output")
+    public String getCommandOutput(@RequestParam String command)
+    {
+        log.info(LOG_TEMPLATE, "GET", "/command-output");
+        return consoleService.getCommandOutput(command);
+    }
+}

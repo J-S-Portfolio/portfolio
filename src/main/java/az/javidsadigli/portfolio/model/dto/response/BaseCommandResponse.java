b3c5f9e8138a375e2d5c79dc52c2a16eeb1ebbfb
Javid Sadigli <cavidoffical@gmail.com>
2025-08-05 00:01:19 +0400

    Added text output model

--- src/main/java/az/javidsadigli/portfolio/model/dto/response/BaseCommandResponse.java
+++ src/main/java/az/javidsadigli/portfolio/model/dto/response/BaseCommandResponse.java
@@ -1,10 +1,12 @@
 package az.javidsadigli.portfolio.model.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 import az.javidsadigli.portfolio.enums.CommandOutputType;
 
 @Data
+@Builder
 public class BaseCommandResponse<T> 
 {
     private T commandOutput;
---
+++ src/main/java/az/javidsadigli/portfolio/model/dto/response/output/TextOutput.java
@@ -0,0 +1,11 @@
+package az.javidsadigli.portfolio.model.dto.response.output;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class TextOutput 
+{
+    private String text; 
+}

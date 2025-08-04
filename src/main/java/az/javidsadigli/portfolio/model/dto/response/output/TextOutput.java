7a4ca4d913c1e3a80ff4448210bacb3bf03de495
Javid Sadigli <cavidoffical@gmail.com>
2025-08-05 00:44:34 +0400

    Added new models

--- src/main/java/az/javidsadigli/portfolio/model/dto/response/output/TextOutput.java
+++ src/main/java/az/javidsadigli/portfolio/model/dto/response/output/TextOutput.java
@@ -2,10 +2,12 @@ package az.javidsadigli.portfolio.model.dto.response.output;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
-public class TextOutput 
+@EqualsAndHashCode(callSuper = false)
+public class TextOutput extends BaseCommandOutput
 {
     private String text; 
 }

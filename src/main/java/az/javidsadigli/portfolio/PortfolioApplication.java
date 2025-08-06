3918033d185b93aad6d5a4ba2926819394b3040e
Javid Sadigli <cavidoffical@gmail.com>
2025-08-07 00:18:37 +0400

    Made main application class more good-looking

--- src/main/java/az/javidsadigli/portfolio/PortfolioApplication.java
+++ src/main/java/az/javidsadigli/portfolio/PortfolioApplication.java
@@ -8,10 +8,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
-public class PortfolioApplication {
-
-	public static void main(String[] args) {
+public class PortfolioApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
-
 }

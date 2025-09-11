7df9e763bdd7663836d1c99b563ca7f831c3e7fc
Javid Sadigli <cavidoffical@gmail.com>
2025-09-11 21:19:04 +0400

    Added new feature to frontend too

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -6,14 +6,27 @@ let currentInput = null;
 let commandHistory = [];
 let currentCommandIndex = 0, lastCommandIndex = 0;
 
+let emailSendingMode = false; 
+let emailPrompts = [
+    "Enter your full name: ", 
+    "Enter your email address: ", 
+    "Enter your phone number: ",
+    "Enter the subject: ", 
+    "Enter your message: ",
+    "Are you sure you want to send the email? (yes/no): "
+];
+let emailKeys = ["userFullName", "userEmail", "userPhone", "subject", "body"];
+let emailData = [];
+
 function addPrompt() 
 {
     const line = document.createElement("div");
     line.className = "line";
 
+    const promptTextContent = emailSendingMode ? emailPrompts[emailData.length] : terminalPromptText;
     const prompt = document.createElement("span");
     prompt.className = "prompt";
-    prompt.textContent = terminalPromptText;
+    prompt.textContent = promptTextContent;
 
     const inputWrapper = document.createElement("span");
     inputWrapper.className = "input-wrapper";
@@ -28,18 +41,59 @@ function addPrompt()
         if (e.key === "Enter") 
         {
             e.preventDefault(); // Prevent newline insertion
-            const command = inputArea.innerText.trim();
-            line.innerHTML = `<span class="prompt">${terminalPromptText}${command}</span>`;
+            const input = inputArea.innerText.trim();
+            line.innerHTML = `<span class="prompt">${promptTextContent}${input}</span>`;
 
-            currentCommandIndex = lastCommandIndex;
-
-            if(commandHistory[lastCommandIndex - 1] != command)
+            if(emailSendingMode)
             {
-                commandHistory.push(command);
-                currentCommandIndex = ++lastCommandIndex;
+                if(emailData.length < emailPrompts.length - 1) 
+                {
+                    emailData.push(input);
+                }
+                else 
+                {
+                    emailSendingMode = false;
+                    if(input.toLowerCase() === "yes")
+                    {
+                        const requestBody = prepareEmailRequestBody(emailKeys, emailData);
+                        sendEmail(requestBody)
+                            .then(response => {
+
+                            }); 
+                        
+                        const output = document.createElement("div");
+                        output.textContent = "Email has been sent successfully.";
+                        terminal.appendChild(output);
+                        emailData = [];
+                        addPrompt();
+                        return; 
+                    }
+                    else 
+                    {
+                        const output = document.createElement("div");
+                        output.textContent = "Email sending cancelled.";
+                        terminal.appendChild(output);
+                        emailData = [];
+                        addPrompt();
+                        return;
+                    }
+                }
+            }
+            else 
+            {
+                currentCommandIndex = lastCommandIndex;
+
+                if(commandHistory[lastCommandIndex - 1] != input)
+                {
+                    commandHistory.push(input);
+                    currentCommandIndex = ++lastCommandIndex;
+                }
             }
 
-            runCommand(command);
+            if(!emailSendingMode)
+                runCommand(input);
+            else 
+                addPrompt();
         }
 
         if(e.key === "ArrowUp")
@@ -121,6 +175,10 @@ function runCommand(cmd)
                         cmdResponse.commandOutput.action.fileName
                     );
                 }
+                else if(cmdResponse.commandOutput.actionType === "SEND_EMAIL")
+                {
+                    emailSendingMode = true;
+                }
             }
 
             addPrompt();
@@ -131,6 +189,32 @@ async function getCommandOutput(cmd)
 {
     const response = await fetch(`/api/console/command-output?command=${cmd.trim()}`)
     return response.json();
+}
+
+async function sendEmail(body)
+{
+    const response = await fetch
+    (
+        '/api/mail/send-email', 
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', "Accept": "*/*" },
+            body: JSON.stringify(body)
+        }
+    );
+
+    return response.json();
+}
+
+function prepareEmailRequestBody(keys, values)
+{
+    const body = {};
+    for(let i = 0; i < keys.length; i++)
+    {
+        body[keys[i]] = values[i];
+        console.log(body);
+    }
+    return body;
 }
 
 function downloadFile(url, filename) 
